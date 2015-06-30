0053276df153693ceef8508dfc29f87c81248bdd
Idroy <Idroy@users.noreply.github.com>
2015-07-01 00:52:44 +0200

    modified:   second.sh

--- second.sh
+++ second.sh
@@ -1,9 +1,11 @@
  if [ "$(id -u)" = "0" ]; then
                 apt-get update
 		apt-get upgrade
-		apt-get install vim clementine plank keepassx 
+		apt-get install vim clementine plank keepassx gdebi-core rar unrar ubuntu-restricted-extras
 		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-		sudo dpkg -i google-chrome*.deb
+		dpkg -i google-chrome*.deb
+		wget http://media.steampowered.com/client/installer/steam.deb
+		gdebi steam.deb
 		 echo "Done"          
                  exit 0
  else
