f028e2bcffbefa92e8e79d3ab8dde448a0e58ecb
Idroy <Idroy@users.noreply.github.com>
2015-07-01 20:26:35 +0200

    modified:   first.sh 	modified:   second.sh

--- first.sh
+++ first.sh
@@ -1,24 +1,24 @@
 if [ "$(id -u)" = "0" ]; then
 		echo "/dev/sda2 /mnt/windows ntfs-3g defaults 0 0" >> /etc/fstab
-		rm -rf /home/yordi/Downloads
-		rm -rf /home/yordi/Pictures
-		rm -rf /home/yordi/Music
-		rm -rf /home/yordi/Documents
-		rm -rf /home/yordi/Videos
-		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Downloads /home/yordi/
-		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Pictures /home/yordi/
-		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Music /home/yordi/
-		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Documents /home/yordi/
-		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Videos /home/yordi/
+		rm -rf $HOME/Downloads
+		rm -rf $HOME/Pictures
+		rm -rf $HOME/Music
+		rm -rf $HOME/Documents
+		rm -rf $HOME/Videos
+		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Downloads $HOME/
+		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Pictures $HOME/
+		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Music $HOME/
+		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Documents $HOME/
+		ln -s /mnt/windows/Documents\ and\ Settings/Yordi/Videos $HOME/
 		apt-get update
 		apt-get upgrade
 		apt-get install vim clementine plank keepassx gdebi-core rar unrar ubuntu-restricted-extras git
 		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb http://media.steampowered.com/client/installer/steam.deb
 		dpkg -i google-chrome*.deb
 		gdebi steam.deb
-		cp /home/yordi/install/caja.desktop /usr/share/applications/caja.desktop
-       	cp /home/yordi/install/mate-terminal.desktop /usr/share/applications/mate-terminal.desktop
-        cp /home/yordi/install/google-chrome.desktop /usr/share/applications/google-chrome.desktop       
+		cp $HOME/install/caja.desktop /usr/share/applications/caja.desktop
+       		cp $HOME/install/mate-terminal.desktop /usr/share/applications/mate-terminal.desktop
+        	cp $HOME/install/google-chrome.desktop /usr/share/applications/google-chrome.desktop       
 		apt-get -f install		
 		echo "Done"          
 		exit 0
--- second.sh
+++ second.sh
@@ -1,14 +1,14 @@
 git clone https://github.com/numixproject/numix-icon-theme-circle
 git clone https://github.com/numixproject/numix-icon-theme
 git clone https://github.com/numixproject/numix-folders
-cp -r /home/yordi/numix-icon-theme-circle/Numix-Circle /home/yordi/.icons/Numix-Circle
-cp -r /home/yordi/numix-icon-theme-circle/Numix-Circle-Light /home/yordi/.icons/Numix-Circle-Light
-cp -r /home/yordi/numix-icon-theme/Numix /home/yordi/.icons/Numix
-cp -r /home/yordi/numix-icon-theme/Numix-Light /home/yordi/.icons/Numix-Light
-cp -r /home/yordi/install/plank /home/yordi/.config/plank
-mkdir /home/yordi/.vim
-mkdir /home/yordi/.vim/undodir
-cp /home/yordi/install/.vimrc /home/yordi/.vimrc
-cp -r /home/yordi/install/Numix /home/yordi/.local/share/plank/themes/Numix
+cp -r $HOME/numix-icon-theme-circle/Numix-Circle $HOME/.icons/Numix-Circle
+cp -r $HOME/numix-icon-theme-circle/Numix-Circle-Light $HOME/.icons/Numix-Circle-Light
+cp -r $HOME/numix-icon-theme/Numix $HOME/.icons/Numix
+cp -r $HOME/numix-icon-theme/Numix-Light $HOME/.icons/Numix-Light
+cp -r $HOME/install/plank $HOME/.config/plank
+mkdir $HOME/.vim
+mkdir $HOME/.vim/undodir
+cp $HOME/install/.vimrc $HOME/.vimrc
+cp -r $HOME/install/Numix $HOME/.local/share/plank/themes/Numix
 echo "Done"
 exit 0
