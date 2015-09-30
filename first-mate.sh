3f931498a8964343e0b2fe786933b5db3a016fc4
Yordi <yordidegraaf@gmail.com>
2015-09-30 23:12:34 +0200

    Changes to be committed: 	modified:   first-mate.sh

--- first-mate.sh
+++ first-mate.sh
@@ -16,9 +16,9 @@ if [ "$(id -u)" = "0" ]; then
 		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb http://media.steampowered.com/client/installer/steam.deb
 		dpkg -i google-chrome*.deb
 		gdebi steam.deb
-		cp $HOME/install/caja.desktop /usr/share/applications/caja.desktop
-       		cp $HOME/install/mate-terminal.desktop /usr/share/applications/mate-terminal.desktop
-        	cp $HOME/install/google-chrome.desktop /usr/share/applications/google-chrome.desktop       
+		cp /home/yordi/install/caja.desktop /usr/share/applications/caja.desktop
+       		cp /home/yordi/install/mate-terminal.desktop /usr/share/applications/mate-terminal.desktop
+        	cp /home/yordi/install/google-chrome.desktop /usr/share/applications/google-chrome.desktop       
 		apt-get -f install		
 		echo "Done"          
 		exit 0
